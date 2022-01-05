917d05bc6a5291b0fd3a8996e191e273f00a5086
pranavgulavani <CORP\pranav.gulavani@a-1dljd3b4xyqbm.corp.amazonworkspaces.com>
2022-01-05 14:21:50 +0530

    concat two strings

--- strings.py
+++ strings.py
@@ -8,7 +8,14 @@ class strings:
         except:
             print("exception occured enter string input")
         
-
+    def concat(self,value1,value2)-> str:
+        try:
+            value3 = str(value1) + str(value2)
+            return value3
+        except:
+            print("Check your input")
+            
 
 val = strings()
-print(val.removeChar('Hello','l','o'))
+#print(val.removeChar('Hello','l','o'))
+print(val.concat('Hel','lo'))
