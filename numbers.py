b53380cc9da0653324c582b9fa7174f48e91c2a1
pranavgulavani <CORP\pranav.gulavani@a-1dljd3b4xyqbm.corp.amazonworkspaces.com>
2022-01-05 18:41:35 +0530

    GCD LCM

--- numbers.py
+++ numbers.py
@@ -1,3 +1,4 @@
+
 class numbers:
     # Check Palindrome for string and numbers 
     def checkPalindrome(self,value):
@@ -13,21 +14,35 @@ class numbers:
             print('Please enter valid number floats and booleans are not allowed')
             
     
-
+    #Palindrome for number
     def checkForNumber(self,value):       
+        reverse = self.reverse(value)
+        if value == reverse:
+            print('Value is palindrome')
+        else:
+            print('Value is not palindrome')
+    
+    #reverse a number
+    def reverse(self,value)->int:
         reverse = 0
         check = value
         while check != 0: 
             digit = check % 10
             reverse = reverse * 10 + digit       
-            check = check//10   
-        if value == reverse:
-            print('Value is palindrome')
-        else:
-            print('Value is not palindrome')
-
-
-
+            check = check//10
+         
+        return reverse
+    def GCD(self,val1,val2):
+        if val2==0:
+            return val1
+        return self.GCD(val2,val1%val2)
+    
+    def LCM(self ,val1,val2):
+        return val1*val2/self.GCD(val1,val2)
+        
 
 num = numbers()
-num.checkPalindrome('xyx')
+#num.checkPalindrome(121)
+#num.reverse(113)
+print(num.GCD(18,12))
+print(num.LCM(30,20))
--- strings.py
+++ strings.py
@@ -7,15 +7,26 @@ class strings:
             return newValue
         except:
             print("exception occured enter string input")
-        
+
+    #concatinate two strings   
     def concat(self,value1,value2)-> str:
         try:
             value3 = str(value1) + str(value2)
             return value3
         except:
             print("Check your input")
-            
+    
+    #Given a string, reverse each word in the sentence
+    def reverseWord(self,value):
+        newString = value.split()
+        for i in range(len(newString)):
+            newString[i] = newString[i][::-1]
+        
+        return '  '.join(newString)
+          
 
 val = strings()
 #print(val.removeChar('Hello','l','o'))
-print(val.concat('Hel','lo'))
+#print(val.concat('Hel','lo'))
+#value = val.reverseWord("hello")
+#print(value)
